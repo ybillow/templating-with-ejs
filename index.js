1e3cff8f80dd1bd020fd6171c931e9b3e113d8ab
Yonus Billow <98870419+ybillow@users.noreply.github.com>
2023-03-09 05:16:24 +0000

    added change

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+//here's a json file of user data
+var data = require('./data/test.json');
+
+app.get('/', (req, res) => {
+  var title = "Home Page";
+  
+  res.render('pages/index', {'title':title});
+});
+app.get('/about', (req, res) => {
+  var title = "About Page";
+  
+  res.render('pages/about', {'title':title});
+});
+
+app.get('/hobbies', (req, res) => {
+  var title = "Hobbies";
+  
+  res.render('pages/hobbies', {'title':title});
+});
+
+app.get('/contact', (req, res) => {
+  var title = "Contact";
+  
+  res.render('pages/contact', {'title':title});
+});
+
+
+
+//users index is our list page
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
